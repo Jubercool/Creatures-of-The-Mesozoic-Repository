9108649444a612e8368a3af8b6da1569dd467c53
Jubercool <mikhailkrp10@gmail.com>
2026-08-10 18:51:37 +1000

    Menu sound effects
    Added menu sound effects (Hover, press)

--- main_menu.gd
+++ main_menu.gd
@@ -12,11 +12,28 @@ func _process(delta: float) -> void:
 
 
 func _on_play_pressed() -> void:
+	$Press.play()
+	await $Press.finished
 	get_tree().change_scene_to_file("res://homecave.tscn")
 
-func _on_options_pressed() -> void:
-	print("Options pressed")
+func _on_play_mouse_entered() -> void:
+	$Hover.play()
 
+func _on_options_pressed() -> void:
+	$Press.play()
+	await $Press.finished
+	print("Options pressed")
+	
+func _on_options_mouse_entered() -> void:
+	$Hover.play()
 
 func _on_exit_pressed() -> void:
+	$Press.play()
+	await $Press.finished
 	get_tree().quit()
+
+func _on_exit_mouse_entered() -> void:
+	$Hover.play()
+
+func _on_Options_mouse_entered() -> void:
+	$Hover.play()
