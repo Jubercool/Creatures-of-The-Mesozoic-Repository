dd275fcc8c0a04fb7b28c0913f99f94cc3d3d4d2
Jubercool <mikhailkrp10@gmail.com>
2026-08-19 20:20:42 +1000

    Added new creatures, added trees
    We be adding new creatures and adding trees

--- Scripts/creaturestats.gd
+++ Scripts/creaturestats.gd
@@ -2,10 +2,37 @@ extends Node
 
 var creatures = {
 	"ischigualastia":{
+		"max_health":150,
+		"damage":[17],
+		"walk_max_vel":150,
+		"sprint_max_vel":300,
+		"acceleration_base":70,
+		"acceleration_buff":100,
+		"attack_cooldown":[2]
+	},
+	"herrerasaurus":{
+		"max_health":35,
+		"damage":[4],
+		"walk_max_vel":325,
+		"sprint_max_vel":750,
+		"acceleration_base":70,
+		"acceleration_buff":100,
+		"attack_cooldown":[1.5]
+	},
+	"saurosuchus":{
 		"max_health":100,
 		"damage":[25],
-		"walk_max_vel":400,
-		"sprint_max_vel":800,
+		"walk_max_vel":225,
+		"sprint_max_vel":450,
+		"acceleration_base":70,
+		"acceleration_buff":100,
+		"attack_cooldown":[2]
+	},
+	"eoraptor":{
+		"max_health":8,
+		"damage":[1],
+		"walk_max_vel":375,
+		"sprint_max_vel":850,
 		"acceleration_base":70,
 		"acceleration_buff":100,
 		"attack_cooldown":[2]
