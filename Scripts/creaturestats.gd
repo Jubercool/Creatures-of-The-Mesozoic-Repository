4abe6432471c02d4cda81dde323df9eb18b501c6
Bartlebye <0727598@schoolsnet.act.edu.au>
2026-08-19 18:15:31 +1000

    Fixed Animation bug & made spawing better

---
+++ Scripts/creaturestats.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var creatures = {
+	"ischigualastia":{
+		"max_health":100,
+		"damage":[25],
+		"walk_max_vel":400,
+		"sprint_max_vel":800,
+		"acceleration_base":70,
+		"acceleration_buff":100,
+		"attack_cooldown":[2]
+	},
+	"player":{
+		"max_health":100,
+		"damage":[25],
+		"walk_max_vel":400,
+		"sprint_max_vel":900,
+		"acceleration_base":70,
+		"acceleration_buff":100,
+		"attack_cooldown":[2]
+	}
+}
--- Scripts/initial.gd
+++ Scripts/initial.gd
@@ -1,21 +1,21 @@
 extends Node2D
 var enemyscene = preload("res://Scenes/enemy.tscn")
 
+var enemies = [
+	#position, type
+	[Vector2i(1000,0),"ischigualastia"],
+	#[Vector2i(1000,1000),"ischigualastia"],
+	#[Vector2i(0,1000),"ischigualastia"],
+]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var enemy1 = enemyscene.instantiate()
-	enemy1.position=Vector2i(1000,0)
-	#enemy1.health=200
-	enemy1.add_to_group("enemies")
-	add_child(enemy1)
-	var enemy2 = enemyscene.instantiate()
-	enemy2.position=Vector2i(1000,1000)
-	enemy2.add_to_group("enemies")
-	add_child(enemy2)
-	var enemy3 = enemyscene.instantiate()
-	enemy3.position=Vector2i(0,1000)
-	enemy3.add_to_group("enemies")
-	add_child(enemy3)
+	for enemy in enemies:
+		var spawned_enemy = enemyscene.instantiate()
+		spawned_enemy.position = enemy[0]
+		spawned_enemy.type = enemy[1]
+		spawned_enemy.add_to_group("enemies")
+		add_child(spawned_enemy)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
